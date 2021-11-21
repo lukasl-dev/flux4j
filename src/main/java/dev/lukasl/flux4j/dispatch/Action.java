57783faf0220f0ec24a7b3718300f3e6b1d6feb2
lukasl-dev <git@lukasl.dev>
2021-11-21 14:32:34 +0100

    feat(dispatch): add Action interface

---
+++ src/main/java/dev/lukasl/flux4j/dispatch/Action.java
@@ -0,0 +1,8 @@
+package dev.lukasl.flux4j.dispatch;
+
+/**
+ * An {@link Action} is a change that is dispatched by a {@link Dispatcher} to the
+ * {@link dev.lukasl.flux4j.store.Store} in order to mutate its state.
+ */
+public interface Action {
+}
